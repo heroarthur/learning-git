0314336f964b40f7439ca5722063a14087a749a9
karol <19kkarol96@gmail.com>
2018-07-16 20:41:14 +0200

    added 2 new units classes

--- modul_jednostki/jednostki.py
+++ modul_jednostki/jednostki.py
@@ -11,3 +11,12 @@ class Jednostka1Poziomu(jednostka):
 
 
 
+class Jednostka2Poziomu(jednostka):
+    def __init__(self):
+        self.data = []
+
+
+class Jednostka3Poziomu(jednostka):
+    def __init__(self):
+        self.data = []
+
